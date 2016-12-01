7aca09e42b742ff6962f954630d2a1d1c56e54bc
Alok Kulkarni <kulkarni.alok@gmail.com>
2016-12-01 13:34:47 +0530

    Initial

---
+++ src/main/java/com/example/alok/SpringIntegrationBatchApplication.java
@@ -0,0 +1,51 @@
+package com.example.alok;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.core.Pollers;
+import org.springframework.integration.jdbc.JdbcPollingChannelAdapter;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@EnableIntegration
+@EnableBatchProcessing
+@SpringBootApplication
+public class SpringIntegrationBatchApplication {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private JobRepository jobRepository;
+
+	@Bean
+	MessageSource<Object> jdbcMessageSource() {
+		return new JdbcPollingChannelAdapter(this.dataSource, "Select * from User WHERE firstName = 'Avinash' and Status = '0'");
+	}
+
+	@Bean
+	IntegrationFlow pollingFlow() {
+		return IntegrationFlows.from(jdbcMessageSource(),
+				c -> c.poller(Pollers.fixedRate(1000).maxMessagesPerPoll(1)))
+				.handle(System.out::println)
+				.get();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationBatchApplication.class, args);
+	}
+}
+
+
---
+++ src/main/resources/UserDB_User.sql
@@ -0,0 +1,4 @@
+INSERT INTO User VALUES ('Alok','Kulkarni','0');
+INSERT INTO User VALUES ('Sheetal','Kulkarni','1');
+INSERT INTO User VALUES ('Dhriti','Kulkarni','1');
+INSERT INTO User VALUES ('Sarthak','Kulkarni','0');
